5a7025f3b5aefaa9d7c7687514fd1076d594ae43
Weena <akpologun.winner@gmail.com>
2022-09-02 00:20:37 +0100

    mobileView Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,49 @@
-import React, {useContext} from "react";
-import Intro from './components/intro/Intro';
-import Toggle from './components/toggle/Toggle';
+import React, { useContext } from "react";
+import Intro from "./components/intro/Intro";
+import Toggle from "./components/toggle/Toggle";
 import Projects from "./components/projects/Projects";
 import Footer from "./components/footer/Footer";
 import { ThemeContext } from "./Context";
-
+import "./App.css";
 
 function App() {
   const theme = useContext(ThemeContext);
-  const {darkMode} = theme.state;
+  const { darkMode } = theme.state;
 
   return (
-    <div style={{
-      backgroundColor: darkMode ? '#0F2027' : 'white',
-      color: darkMode && 'white',
-      // backgroundImage: darkMode && 'linear-gradient(to right, #0F2027,#203A43,#2C5364)'
-    }}>
+    <>
       <Toggle />
-      <Intro />
-      <Projects />
-      <Footer />
-    </div>
+      <div
+        className="mobile"
+        style={{
+          // backgroundColor: darkMode ? "#0F2027" : "white",
+          color: darkMode && "white",
+          overflowX: "hidden",
+          backgroundImage:
+            darkMode && "linear-gradient(to right, #0F2027,#203A43,#2C5364)"
+        }}
+      >
+        <h1 className="mobile-text" style={{
+          // backgroundColor: darkMode ? "#0F2027" : "white",
+          color: darkMode && "white",
+        }}>MOBILE VIEW NOT AVAILABLE YET. KINDLY VIEW IN DESKTOP</h1>
+      </div>
+      <div
+        className="main"
+        style={{
+          backgroundColor: darkMode ? "#0F2027" : "white",
+          color: darkMode && "white",
+          width: "100vw",
+          overflowX: "hidden"
+          // backgroundImage: darkMode && 'linear-gradient(to right, #0F2027,#203A43,#2C5364)'
+        }}
+      >
+        <Toggle />
+        <Intro />
+        <Projects />
+        <Footer />
+      </div>
+    </>
   );
 }
 
